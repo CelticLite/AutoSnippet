532c96f626df62991f9d2495df2846a2b8f9aef9
Imstadt99 <98971161+Imstadt99@users.noreply.github.com>
2022-03-23 13:00:34 -0400

    Update login.php

--- project/apache/testing/login.php
+++ project/apache/testing/login.php
@@ -4,7 +4,6 @@
 	<meta charset="UTF-8">
 	<title>Auto Anippet Login Page</title>
 	<link rel="stylesheet" href="main.css">
-
 </head>
 <body>
 <div class="wrapper">
@@ -20,36 +19,32 @@
 				</div>
 			</div>
 			<br>
-			<form action="index.php" method="post" id="short_label_form">
-        <input type="hidden" name="action" value="validate_login">
-
-        <h1>Customer Login</h1>
-        <p>You must login before you can register a product</p>
+		<form action="index.php" method="post" id="short_label_form">
+       			<input type="hidden" name="action" value="validate_login">
+        		<h1>Customer Login</h1>
+        		<p>You must login before you can register a product</p>
    
-
-            <label for="id"><b>Username:</b></label>
-            <input type="text" name="id" required/>
+            		<label for="id"><b>Username:</b></label>
+           		<input type="text" name="id" required/>
         
-            <label for="password"><b>Password:</b></label>
-            <input type="text" name="password" required/>
+            		<label for="password"><b>Password:</b></label>
+            		<input type="text" name="password" required/>
 
-            <input type="submit" name="login" id="login" value="Login" />
+            		<input type="submit" name="login" id="login" value="Login" />
             
-        </form>
+        	</form>
 			<div class="or">
 				<div class="line"></div>
 				<p>OR</p>
 				<div class="line"></div>
 			</div>
 
-  <div id="name"></div>
-
-
+  		<div id="name"></div>
 		<div class="signup">
 			<p>Don't have an account? <a href="#">Register</a></p>
 		</div>
 		</div>
-	</div>
+		</div>
 
 	<div class="footer">
 		<div class="links">
