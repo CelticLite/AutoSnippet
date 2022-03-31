301976b407843a8db885ee8c19e140a053130027
Imstadt99 <98971161+Imstadt99@users.noreply.github.com>
2022-03-31 10:52:55 -0400

    Update useradd.php

--- project/apache/testing/view/useradd.php
+++ project/apache/testing/view/useradd.php
@@ -1,62 +1,60 @@
 <html>
 <head>
     <title>Auto Snippet Login Page</title>
-    <link rel="stylesheet" type="text/css" href="main.css"/>
+    <link rel="stylesheet" type="text/css" href="./view/main.css?version=51">
 </head>
 <body>
 
 <div class="wrapper">
-	<div class="header">
-            <div class="top">
+<div class="header">
+<div class="top">
 
-        <div class="container1">
-                <div class="logo">
-                        <img src="ASLogoV1.1.jpeg" alt="AutoSnippet" style="width: 200px; height: 150px;">
-                </div>
-                <div class="welcome">
-                        <p>Welcome to Auto Snippet! <br> Register User:</p>
-                </div>
+    <div class="container1">
+        <div class="logo">
+        <img src="ASLogoV1.1.jpeg" alt="AutoSnippet" style="width: 200px; height: 150px;">
         </div>
-        <div class="form">
+        <div class="welcome">
+        <p>Welcome to Auto Snippet! <br> Register User:</p>
+        </div>
+        </div>
+    <div class="form">
             
-<body>
+    <body>
     <main>
     <h1>Account Sign Up</h1>
-    <form action="index.php" method="post" id="add_user_form">
+    
+    <form action="index.php" method="post" id="short_label_form">
         <input type="hidden" name="action" value="add_user">
        <div class="input_field">
         <label>Username:</label>
         <input type="text" name="username" value="" placeholder = "Username" class="input">
        </div><br>
+
 	<div class="input_field">
         <label>Password:</label>
         <input type="password" name="password" placeholder="Password" class="input">
         </div><br>
+
 	<div class="input_field">
         <label>First Name:</label>
         <input type="text" name="fname" value="" placeholder = "First Name" class="input">
         </div><br>
+
 	<div class="input_field">
         <label>Last Name:</label>
         <input type="text" name="lname" value="" placeholder = "Last Name" class="input">
         </div><br>
+
 	<div class="input_field">
         <label>E-Mail:</label>
         <input type="text" name="email" value="" placeholder = "Email" class="input">
         </div><br>
-	<!-- Moved these to the top bc Username/Password choice should be prompt 1&2
-        <div class="input_field">
-        <label>Username:</label>
-        <input type="text" name="username" value="" placeholder = "Username" class="input">
-        </div><br>
-	<div class="input_field">
-        <label>Password:</label>
-        <input type="password" name="password" placeholder="Password" class="input">
-        </div><br>-->
+
 	<div class="input_field">
         <label>Phone Number:</label>
         <input type="text" name="phone" value="" placeholder = "Phone Number" class="input">
         </div><br>
+
 	<div class="input_field">
         <label>Address:</label>
         <input type="text" name="address" value="" placeholder = "Address" class="input">
@@ -82,23 +80,10 @@
         
         <div class="input_field">
         <label for="teamselect">Choose your team:</label>
-        <select name="team_on" id="teamselect" class="input_field">
-          <option value="teamA">Team A</option>
-          <option value="teamB">Team B</option>
-          <option value="teamC">Team C</option>
-          <option value="teamD">Team D</option>
-        </select>
+
      </div><br>
 
         
-       <!-- <input type="radio" name="team_on" value="TeamA">
-        Team A<br>
-        <input type="radio" name="team_on" value="TeamB">
-        Team B<br>
-        <input type="radio" name="team_on" value="TeamC">
-        Team C<br>
-        <input type="radio" name="team_on" value="TeamD">
-        Team D<br>-->
         <label>&nbsp;</label>
         <div class="btn"><input type="submit" value="Add User" /></div>
         <br>
