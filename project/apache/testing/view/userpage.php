c1abad212a2ffeaadaf4b7adb32c5ec18990ba1b
gzt11 <98411441+gzt11@users.noreply.github.com>
2022-04-18 15:41:48 -0400

    Update userpage.php

--- project/apache/testing/view/userpage.php
+++ project/apache/testing/view/userpage.php
@@ -120,7 +120,7 @@
 
 
 
-      <h2>All Goals</h2><br>
+      <h2>All Goals:</h2><br>
 
         <!-- this is the div divider line for the foreach comments don't delete the line below!-->
         ____________________________________________________________________________________________________________________________________
