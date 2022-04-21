41c39f51b2ba392a732fec7963705316afa9bb0f
gzt11 <98411441+gzt11@users.noreply.github.com>
2022-04-20 22:42:30 -0400

    Update database.php

--- project/apache/testing/model/database.php
+++ project/apache/testing/model/database.php
@@ -1,8 +1,18 @@
 <?php
-$db = new PDO('pgsql:host=localhost;dbname=testdb',"testuser","Infected42!");
-$statement = $db->prepare("SELECT datname FROM pg_database");
-$statement->execute();
-while ($row = $statement->fetch()) {
-    echo "<p>" . htmlspecialchars($row["datname"]) . "</p>\n";
-}
-?>
+    $dsn = 'pgsql:host=localhost;dbname=testdb';
+    $username = 'testuser';
+    $password = 'Infected42!';
+  
+    
+    
+                
+    
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
+        exit();
+    }
+               
