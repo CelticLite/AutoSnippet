5cf32b8e10a9d1d060e837570eb01bd125a77d62
gzt11 <98411441+gzt11@users.noreply.github.com>
2022-04-24 23:09:08 -0400

    Update userpage.php

--- project/php/testing/view/userpage.php
+++ project/php/testing/view/userpage.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Auto Snippet</title>
-    <link rel="stylesheet" href="main.css" />
+    <link rel="stylesheet" href="./view/main.css" />
 </head>
 <body>
 <div class ="bwrapper">
