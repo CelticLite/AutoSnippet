7adf513716ecc017f0cec7271d92b81cbb45e716
gzt11 <98411441+gzt11@users.noreply.github.com>
2022-04-24 23:05:20 -0400

    Update userpagefilterinprogress.php

--- project/php/testing/view/userpagefilterinprogress.php
+++ project/php/testing/view/userpagefilterinprogress.php
@@ -8,50 +8,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Auto Snippet</title>
-    <link rel="stylesheet" href="main.css" />
+    <link rel="stylesheet" href="./view/main.css" />
 </head>
 <body>
 <div class ="bwrapper">
     <!-- Left side bar -->
     <div class="lsidebar">
         <div class="sidebarOption">
-            <h2>Home</h2>
+            <h1>Auto Snippet</h1>
         </div>
 
- 
+
         <br><br><br>
 
 
-       <h2>Filter:</h2>
+        <h2>Filter:</h2>
         <form action="index.php" method="post">
             <input type="hidden" name="action" value ="filter_all">
 
-            
+
             <button type='submit' button class = "newGoal__button">All</button>
         </form>
         <form action="index.php" method="post">
             <input type="hidden" name="action" value ="filter_uncomplete">
 
-           
+
             <button type='submit' button class = "newGoal__button">Not Complete</button>
         </form>
         <form action="index.php" method="post">
             <input type="hidden" name="action" value ="filter_in_progress">
 
-           
+
             <button type='submit' button class = "newGoal__button">In Progress</button>
         </form>
         <form action="index.php" method="post">
             <input type="hidden" name="action" value ="filter_done">
 
-            
+
             <button type='submit' button class = "newGoal__button">Comepleted</button>
         </form>
 
-        <div class="lsidebar__input"><br><br>
-
-           
-        </div>
     </div>
     <!-- Begin Goals Feed -->
     <div class="goals">
@@ -68,8 +64,8 @@
 
             <div class="goals__input">
 
-                <?php
-                echo $_SESSION['username'];?>
+                <h3> Welcome, <?php
+                    echo $_SESSION['username'];?>.</h3>
 
                 <img
                         src="https://i.pinimg.com/originals/a6/58/32/a65832155622ac173337874f02b218fb.png"
@@ -84,13 +80,13 @@
                     <input type="hidden" name="action" value="add_comment">
                     <!-- NEED TO GRAB USER INFO -->
                     <input type='hidden' name='uid' value='<?php
-                        echo $_SESSION['username'];?>''>
+                    echo $_SESSION['username'];?>''>
                     <p>
-                    <select id="status" name="status">
-                        <option value="Not Completed" data-sort="1" font-color='green'>Not Completed</option>
-                        <option value="In Progress" data-sort="2" font-color='yellow'>In Progress</option>
-                        <option value="Completed" data-sort="3" font-color='red'>Completed</option>
-                    </select>
+                        <select id="status" name="status">
+                            <option value="Not Completed" data-sort="1" font-color='green'>Not Completed</option>
+                            <option value="In Progress" data-sort="2" font-color='yellow'>In Progress</option>
+                            <option value="Completed" data-sort="3" font-color='red'>Completed</option>
+                        </select>
                     </p>
 
                     <textarea name="message" rows = "10" cols = "142" Placeholder="What's your new weekly goal?"></textarea>
@@ -109,64 +105,64 @@
 
 
 
-<h2>In Progress Goals:</h2>
-      
+        <h2>In Progress Goals:</h2><br>
+
         <!-- this is the div divider line for the foreach comments don't delete the line below!-->
         ____________________________________________________________________________________________________________________________________
-<br>
+        <br>
 
 
-               <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" type="text/javascript"></script>
-            <script type="text/javascript">
-                function searchAndHighlight(searchTerm, selector) {
-                    if (searchTerm) {
-                        var selector = selector || "#realTimeContents";
-                        var searchTermRegEx = new RegExp(searchTerm, "ig");
-                        var matches = $(selector).text().match(searchTermRegEx);
-                        if (matches != null && matches.length > 0) {
-                            $('.highlighted').removeClass('highlighted');
-                            $span = $('#realTimeContents span');
-                            $span.replaceWith($span.html());
-                            if (searchTerm === "&") {
-                                searchTerm = "&amp;";
-                                searchTermRegEx = new RegExp(searchTerm, "ig");
-                            }
-                            $(selector).html($(selector).html().replace(searchTermRegEx, "<span class='match'>" + searchTerm + "</span>"));
-                            $('.match:first').addClass('highlighted');
-                            var i = 0;
-                            $('#NextBtn').off('click').on('click', function () {
-                                i++;
-                                if (i >= $('.match').length) i = 0;
-                                $('.match').removeClass('highlighted');
-                                $('.match').eq(i).addClass('highlighted');
-                                $('.ui-mobile-viewport').animate({
-                                    scrollTop: $('.match').eq(i).offset().top
-                                }, 300);
-                            });
-                            $('#PreBtn').off('click').on('click', function () {
-                                i--;
-                                if (i < 0) i = $('.match').length - 1;
-                                $('.match').removeClass('highlighted');
-                                $('.match').eq(i).addClass('highlighted');
-                                $('.ui-mobile-viewport').animate({
-                                    scrollTop: $('.match').eq(i).offset().top
-                                }, 300);
-                            });
-                            if ($('.highlighted:first').length) { //if match found, scroll to where the first one appears
-                                $(window).scrollTop($('.highlighted:first').position().top);
-                            }
-                            return true;
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            function searchAndHighlight(searchTerm, selector) {
+                if (searchTerm) {
+                    var selector = selector || "#realTimeContents";
+                    var searchTermRegEx = new RegExp(searchTerm, "ig");
+                    var matches = $(selector).text().match(searchTermRegEx);
+                    if (matches != null && matches.length > 0) {
+                        $('.highlighted').removeClass('highlighted');
+                        $span = $('#realTimeContents span');
+                        $span.replaceWith($span.html());
+                        if (searchTerm === "&") {
+                            searchTerm = "&amp;";
+                            searchTermRegEx = new RegExp(searchTerm, "ig");
                         }
+                        $(selector).html($(selector).html().replace(searchTermRegEx, "<span class='match'>" + searchTerm + "</span>"));
+                        $('.match:first').addClass('highlighted');
+                        var i = 0;
+                        $('#NextBtn').off('click').on('click', function () {
+                            i++;
+                            if (i >= $('.match').length) i = 0;
+                            $('.match').removeClass('highlighted');
+                            $('.match').eq(i).addClass('highlighted');
+                            $('.ui-mobile-viewport').animate({
+                                scrollTop: $('.match').eq(i).offset().top
+                            }, 300);
+                        });
+                        $('#PreBtn').off('click').on('click', function () {
+                            i--;
+                            if (i < 0) i = $('.match').length - 1;
+                            $('.match').removeClass('highlighted');
+                            $('.match').eq(i).addClass('highlighted');
+                            $('.ui-mobile-viewport').animate({
+                                scrollTop: $('.match').eq(i).offset().top
+                            }, 300);
+                        });
+                        if ($('.highlighted:first').length) { //if match found, scroll to where the first one appears
+                            $(window).scrollTop($('.highlighted:first').position().top);
+                        }
+                        return true;
                     }
-                    return false;
                 }
-                $(document).on('click', '#SearchBtn', function (event) {
-                    $(".highlighted").removeClass("highlighted").removeClass("match");
-                    if (!searchAndHighlight($('#SearchTxt').val())) {
-                        alert("No results found");
-                    }
-                });
-            </script>
+                return false;
+            }
+            $(document).on('click', '#SearchBtn', function (event) {
+                $(".highlighted").removeClass("highlighted").removeClass("match");
+                if (!searchAndHighlight($('#SearchTxt').val())) {
+                    alert("No results found");
+                }
+            });
+        </script>
 
 
         <body>
@@ -186,9 +182,9 @@
             </form>
         </form>
 
-            
 
-                <div class = "feed" id="realTimeContents">
+
+        <div class = "feed" id="realTimeContents">
             <table id="table_example">
                 <table>
                     <tbody>
@@ -203,7 +199,7 @@
 
                                 <?php echo "Comment ID: ".$comment['cid'];?><br>
                                 <?php echo "Status: ".$comment['status'];?><br>
-                            <?php
+                                <?php
                                 echo "Username: ".$comment['uid'];?><br><br>
 
 
@@ -212,27 +208,19 @@
 
 
 
-                              <!--<form method="post" action="index.php">
-                                   <input type="hidden" name="action" value ="edit_comment">
-
-                                   <input type="hidden" name="cid"
-                                          value="?php echo $comment['cid']; ?>">
-
-                                   <input type="submit" value="Edit">
-                               </form>
-
-
-
-                               <form action="index.php" method="post">
-                                   <input type="hidden" name="action" value ="delete_comment">
-                                   <input type="hidden" name="cid"
-                                          value="?php echo $comment['cid']; ?>">
-
-                                   <input type="submit" value="Delete">
-                               </form>
-
-
-                               <button onclick="replyFunction()" button class = "newGoal__button">Reply</button>-->
+                                <!--<form method="post" action="index.php">
+                                     <input type="hidden" name="action" value ="edit_comment">
+                                     <input type="hidden" name="cid"
+                                            value="?php echo $comment['cid']; ?>">
+                                     <input type="submit" value="Edit">
+                                 </form>
+                                 <form action="index.php" method="post">
+                                     <input type="hidden" name="action" value ="delete_comment">
+                                     <input type="hidden" name="cid"
+                                            value="?php echo $comment['cid']; ?>">
+                                     <input type="submit" value="Delete">
+                                 </form>
+                                 <button onclick="replyFunction()" button class = "newGoal__button">Reply</button>-->
 
                             <form action="index.php" method="post">
                                 <input type='hidden' name='uid' value='$comment["uid"]'>
@@ -262,7 +250,7 @@
                             </form>
 
 
-                          
+
 
 
 
